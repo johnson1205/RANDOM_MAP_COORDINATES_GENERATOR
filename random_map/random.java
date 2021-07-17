f05072fdff2190fc9e5941133bfc214abf500947
Black_block <46107558+johnson1205@users.noreply.github.com>
2021-07-17 15:00:49 +0800

    Update random.java

--- random_map/random.java
+++ random_map/random.java
@@ -8,6 +8,7 @@ public class random {
 		int a1, b1;
 		int a2, b2;
 		int a3, b3;
+		int coor;
 		for(int i = 0; i < 5; i++){
 			a = (int)(180-(Math.random()*360));
 			b = (int)(90-(Math.random()*180));
@@ -16,18 +17,37 @@ public class random {
 			a2 = (int)(Math.random()*60);
 			b2 = (int)(Math.random()*60);
 			a3 = (int)(Math.random()*60);
-			b3= (int)(Math.random()*60);
+			b3 = (int)(Math.random()*60);
+			coor = (int)(Math.random()*2);
 			if(a > 0){
-				System.out.println(Math.abs(a)+"줩" +a1+"'"+a2+"."+a3+ '"' + "E");
+				System.out.println(Math.abs(a)+"째" +a1+"'"+a2+"."+a3+ '"' + "E");
+			}
+			else if(a == 0){
+				System.out.printf(Math.abs(a)+"째" +a1+"'"+a2+"."+a3+ '"');
+				if(coor == 0){
+					System.out.println("E");
+				}
+				else if(coor == 1){
+					System.out.println("W");
+				}
 			}
 			else if(a < 0){
-				System.out.println(Math.abs(a)+"줩" +a1+"'"+a2+"."+a3+ '"' + "W");
+				System.out.println(Math.abs(a)+"째" +a1+"'"+a2+"."+a3+ '"' + "W");
 			}
 			if(b > 0){
-				System.out.println(Math.abs(b)+"줩" +b1+"'"+b2+"."+b3+ '"' + "N");
+				System.out.println(Math.abs(b)+"째" +b1+"'"+b2+"."+b3+ '"' + "N");
+			}
+			else if(b == 0){
+				System.out.printf(Math.abs(a)+"째" +b1+"'"+b2+"."+b3+ '"');
+				if(coor == 0){
+					System.out.println("N");
+				}
+				else if(coor == 1){
+					System.out.println("S");
+				}
 			}
 			else if(b < 0){
-				System.out.println(Math.abs(b)+"줩" +b1+"'"+b2+"."+b3+ '"' + "S");
+				System.out.println(Math.abs(b)+"째" +b1+"'"+b2+"."+b3+ '"' + "S");
 			}
 			System.out.println();
 		}
